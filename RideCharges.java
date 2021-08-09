d06667bcaaf24c4c7f825b720d9d08e1ddc35860
afrozbasha <iafrozbasha@gmail.com>
2021-08-09 21:03:46 +0530

    Calculate Total Fare with no.of rides with UserId

--- RideCharges.java
+++ RideCharges.java
@@ -1,24 +1,21 @@
 package com.CabInvoiceGenerator;
 
 public class RideCharges {
-    public static int MinimumCostPerKM = 10;
-    public static int CoastPerMinute = 1;
-    public static int MinimumFare = 5;
+    public int MinimumCostPerKM = 10;
+    public int CoastPerMinute = 1;
+    public int MinimumFare = 5;
 }
 
 class Ride extends RideCharges {
     public double distance;
     public int time;
-
-    public Ride(double distance, int time) {
+    public String userId;
+    public Ride(double distance, int time, String userId) {
         this.distance = distance;
         this.time = time;
+        this.userId = userId;
     }
-
-    public double CalculateFare() {
-        double totalFare = (distance * MinimumCostPerKM) + (time * CoastPerMinute);
-        return Math.max(MinimumFare, totalFare);
-    }
-
+    public  Ride(){};
 }
+
 
--- RideMain.java
+++ RideMain.java
@@ -1,31 +1,55 @@
 package com.CabInvoiceGenerator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class RideMain {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Ride userCab = new Ride();
 
         ArrayList<Ride> cab = new ArrayList<>();
         System.out.println("Enter how many ride's want to travel");
         int n = sc.nextInt();
+        String userId = "";
         int temp = n;
         while (temp!=0) {
+            System.out.println("Enter user Id : ");
+            userId = sc.next();
             System.out.println("Enter the how much distance to travel: ");
             double distance = sc.nextDouble();
             System.out.println("Enter the how much time to travel: ");
             int time = sc.nextInt();
-            Ride userCab = new Ride(distance, time);
+            userCab = new Ride(distance, time, userId);
             cab.add(userCab);
             temp--;
         }
         double totalFare = 0;
+        float avg = 1;
+        double v = 0;
+        ArrayList<Invoice> invoices = new ArrayList<Invoice>();
         for (Ride r: cab){
-            totalFare +=  (r.distance * r.MinimumCostPerKM) + (r.time * r.CoastPerMinute);
+            v += (r.distance * r.MinimumCostPerKM) + (r.time * r.CoastPerMinute);
+            totalFare += Math.max(userCab.MinimumCostPerKM, v);
+            avg = (float) totalFare / n;
+            invoices.add(new Invoice(r.userId , totalFare, avg, n));
         }
 
-        System.out.println("The total Fare "+totalFare+" for "+n+ " no.of ride's");
+
+
+        Iterator<Invoice> invoiceIterator = invoices.iterator();
+        while (invoiceIterator.hasNext()){
+            Invoice s = invoiceIterator.next();
+
+            System.out.println("User Is is : "+s.uderId);
+            System.out.println("The total Fare is :"+s.totalFare);
+            System.out.println("No.of ride's is : "+s.noOfRides);
+            System.out.println("Average fare for each ride is : "+s.avg);
+
+        }
+
+
     }
 }
